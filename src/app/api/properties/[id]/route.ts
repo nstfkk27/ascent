7e3de7914121fa6fad6ff250fd6f93a5b96423af
nstfkk27 <nstfkk@gmail.com>
2025-12-12 23:29:59 +0700

    Debug all the previous push

--- src/app/api/properties/[id]/route.ts
+++ src/app/api/properties/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
+import { sanitizePropertyData } from '@/lib/property-utils';
 
 const prisma = new PrismaClient();
 
